f03384a764c01a34f36c25624f032af8404ddd4a
Kaiser, Anton <anton.kaiser@study.thws.de>
2023-11-08 00:33:52 +0100

    refactoring and improve a bit, add line animation

--- map/plot_gps_history.py
+++ map/plot_gps_history.py
@@ -1,55 +1,57 @@
 import folium
 from folium.plugins import TimestampedGeoJson
+import datetime
 
-# Your list of GPS positions with timestamps
-gps_positions_with_time = [
-    {'time': '2023-01-01T00:00:00', 'coordinates': (6.960279, 50.937531, )},  # Cologne Cathedral
-    {'time': '2023-01-01T01:00:00', 'coordinates': (2.294481, 48.858370)},  # Eiffel Tower
-    # Add more dictionaries with 'time' and 'coordinates' keys here
-]
-
-# Create a map object, starting at the first position
-mymap = folium.Map(location=gps_positions_with_time[0]['coordinates'], zoom_start=5)
-
-# Create a features list for the TimestampedGeoJson
-features = [
-    {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': point['coordinates'],
-        },
-        'properties': {
-            'time': point['time'],
-            'popup': point['time'],  # Use the timestamp as a popup content
-            'id': 'house',
-            'icon': 'circle',
-            'iconstyle': {
-                'fillColor': '#ff0000',
-                'fillOpacity': 0.8,
-                'stroke': 'true',
-                'radius': 7
+# Function to create a feature for each segment of the journey
+def create_geojson_features(data):
+    features = []
+    for i in range(len(data) - 1):
+        feature = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'LineString',
+                'coordinates': [data[i]['coordinates'], data[i+1]['coordinates']],
             },
-        },
-    } for point in gps_positions_with_time
-]
+            'properties': {
+                'times': [data[i]['time'], data[i+1]['time']],
+                'style': {
+                    'color': 'red',
+                    'weight': 5
+                }
+            }
+        }
+        features.append(feature)
+    return features
 
-# Add the features to the map with a time slider
-TimestampedGeoJson({
-    'type': 'FeatureCollection',
-    'features': features,
-}, period='PT1H',  # Set the period for the timeslider here, 'PT1H' is 1 hour
-    add_last_point=True,
-    auto_play=False,
-    loop=False,
-    max_speed=1,
-    loop_button=True,
-    date_options='YYYY/MM/DD HH:mm:ss',
-    time_slider_drag_update=True).add_to(mymap)
+if __name__ == '__main__':
+    # Sample data: list of coordinates with timestamps
+    data = [
+        {'coordinates': [6.960279, 50.937531], 'time': '2023-01-01T00:00:00Z'}, # Cologne Cathedral
+        {'coordinates': [2.294481, 48.858370], 'time': '2023-01-01T01:00:00Z'}, # Eiffel Tower
+        {'coordinates': [3.294481, 48.858370], 'time': '2023-01-01T02:00:00Z'},
+        {'coordinates': [4.294481, 48.858370], 'time': '2023-01-01T03:00:00Z'},
+        {'coordinates': [5.294481, 47.858370], 'time': '2023-01-01T04:00:00Z'},
+        # Add more entries here
+    ]
+    # Create map object
+    m = folium.Map(location=data[0]['coordinates'], zoom_start=3)
 
-# Save it to an html file
-mymap.save('my_timed_map.html')
+    # Create features
+    features = create_geojson_features(data)
 
-# This will open the map in your web browser
-import webbrowser
-webbrowser.open('my_timed_map.html')
+    # Create a TimestampedGeoJson object and add it to the map
+    TimestampedGeoJson({
+        'type': 'FeatureCollection',
+        'features': features,
+    }, period='PT30M',
+        transition_time=1000,
+        auto_play=True,
+        loop=True,
+        max_speed=1,
+        loop_button=True,
+        date_options='YYYY/MM/DD HH:mm:ss',
+        time_slider_drag_update=True,
+        add_last_point=True).add_to(m)
+
+    # Save to an HTML file
+    m.save('animated_path.html')
